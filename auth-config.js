6312a4cbd3a50671546af90968e3336bae3b5621
milesnavarez30-eng <milesnavarez30@gmail.com>
2026-09-02 12:43:00 +0800

    Create auth-config.js

---
+++ auth-config.js
@@ -0,0 +1,11 @@
+/*
+CCTV OPS ACCOUNT CONFIG
+
+Public Supabase frontend credentials only.
+Do NOT put a service_role or secret key here.
+*/
+
+window.CCTV_AUTH_CONFIG = {
+    SUPABASE_URL: "https://afxgfyuudqujueeooplj.supabase.co",
+    SUPABASE_PUBLIC_KEY: "sb_publishable_JGUCURJkbOw-KypucxXNSQ_magKTFPd"
+};
